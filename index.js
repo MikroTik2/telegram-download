14464cff71d51ba3b337482e902c2214ca7f9b32
Артур Доценко <arturdocenko@MacBook-Air-Artur.local>
2023-12-12 04:26:52 +0200

    fix index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const { Telegraf, session } = require("telegraf");
 const dotenv = require("dotenv").config();
 
+const port = process.env.PORT || 3000;
+
 const startComposer = require("./composers/start.command.js");
 const ytdlComposer = require("./composers/ytdl.command.js");
 
 const bot = new Telegraf(process.env.TOKEN_BOT);
 
-bot.launch({ webhook: { domain: 'https://telegram-download.vercel.app/', port: 4000 } })
+bot.launch({ webhook: { domain: 'https://telegram-download.vercel.app/', port } })
 
 bot.use(session());
 
